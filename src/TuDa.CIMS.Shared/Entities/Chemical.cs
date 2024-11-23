080d0ddf38d21f943836a4f1f1089098387307f1
david.schmidt1 <davii.schmidt@stud.tu-darmstadt.de>
2024-11-23 15:23:21 +0100

    Add record class "Chemical" and "Consumables"

---
+++ src/TuDa.CIMS.Shared/Entities/Chemical.cs
@@ -0,0 +1,24 @@
+﻿namespace TuDa.CIMS.Shared.Entities;
+
+/// <summary>
+/// An entity representing a chemical.
+/// </summary>
+public record Chemical()
+{
+    /// <summary>
+    /// An unique identifier for the chemical.
+    /// </summary>
+    public required string Cas { get; set; }
+    /// <summary>
+    /// A list of hazards associated with the chemical.
+    /// </summary>
+    public List<Hazard> Hazards { get; set; } = [];
+    /// <summary>
+    /// The unit of the chemical.
+    /// </summary>
+    public required string Unit { get; set; }
+    /// <summary>
+    /// The item number of the chemical.
+    /// </summary>
+    public required string ItemNumber { get; set; }
+};
---
+++ src/TuDa.CIMS.Shared/Entities/Consumables.cs
@@ -0,0 +1,20 @@
+﻿namespace TuDa.CIMS.Shared.Entities;
+
+/// <summary>
+/// An entity representing a consumable item in the chemical inventory.
+/// </summary>
+public record Consumables()
+{
+    /// <summary>
+    /// The name of the consumable item.
+    /// </summary>
+    public required string Manufacturer { get; set; }
+    /// <summary>
+    /// The serial number of the consumable item.
+    /// </summary>
+    public required string SerialNumber { get; set; }
+    /// <summary>
+    /// The item number of the consumable item.
+    /// </summary>
+    public required string ItemNumber { get; set; }
+};
