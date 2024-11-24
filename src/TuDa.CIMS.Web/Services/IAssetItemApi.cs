86cfd2eacd2f27ba96333e63bc17cdf727d7c27e
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2024-11-24 18:44:13 +0100

    Create IAssetItemApi

---
+++ src/TuDa.CIMS.Web/Services/IAssetItemApi.cs
@@ -0,0 +1,24 @@
+﻿using Refit;
+using TuDa.CIMS.Shared.Attributes.ServiceRegistration;
+using TuDa.CIMS.Shared.Entities;
+
+namespace TuDa.CIMS.Web.Services;
+
+[RefitClient("/api/asset-items")]
+public interface IAssetItemApi
+{
+    [Get("/{id}")]
+    public Task<AssetItem> GetAsync(Guid id);
+
+    [Get("/")]
+    public Task<IEnumerable<AssetItem>> GetAllAsync();
+
+    [Put("/")]
+    public Task<Guid> CreateAsync(AssetItem item);
+
+    [Get("/{id}")]
+    public Task RemoveAsync(Guid id);
+
+    [Patch("/{id}")]
+    public Task<AssetItem> UpdateAsync(Guid id, AssetItem item);
+}
