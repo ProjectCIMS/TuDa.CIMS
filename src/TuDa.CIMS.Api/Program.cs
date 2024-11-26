291ddc70d3cff4517497edc84ec82cce62f92762
david.schmidt1 <davii.schmidt@stud.tu-darmstadt.de>
2024-11-26 20:10:35 +0100

    removed empty line

--- src/TuDa.CIMS.Api/Program.cs
+++ src/TuDa.CIMS.Api/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.ConfigureDbContext(builder.Configuration);
 
 
-
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -49,4 +48,3 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
-
