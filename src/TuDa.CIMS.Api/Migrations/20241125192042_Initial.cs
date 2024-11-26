4a6a77082830740496a351b53037a58b6ac3dcb0
David Schmidt <122273032+devidsdt@users.noreply.github.com>
2024-11-26 20:16:27 +0100

    Dbcontext with Assetitem (#42)
    Co-authored-by: david.schmidt1 <davii.schmidt@stud.tu-darmstadt.de>
    Co-authored-by: Marlon Rosenberg <59970382+Sc3l3t0n@users.noreply.github.com>

---
+++ src/TuDa.CIMS.Api/Migrations/20241125192042_Initial.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TuDa.CIMS.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssetItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Note = table.Column<string>(type: "text", nullable: false),
+                    RoomId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ItemNumber = table.Column<string>(type: "text", nullable: false),
+                    Shop = table.Column<string>(type: "text", nullable: false),
+                    Discriminator = table.Column<string>(type: "character varying(13)", maxLength: 13, nullable: false),
+                    Cas = table.Column<string>(type: "text", nullable: true),
+                    Unit = table.Column<string>(type: "text", nullable: true),
+                    Manufacturer = table.Column<string>(type: "text", nullable: true),
+                    SerialNumber = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssetItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssetItems_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hazards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ImagePath = table.Column<string>(type: "text", nullable: false),
+                    ChemicalId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hazards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Hazards_AssetItems_ChemicalId",
+                        column: x => x.ChemicalId,
+                        principalTable: "AssetItems",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetItems_RoomId",
+                table: "AssetItems",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hazards_ChemicalId",
+                table: "Hazards",
+                column: "ChemicalId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hazards");
+
+            migrationBuilder.DropTable(
+                name: "AssetItems");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
--- src/TuDa.CIMS.Api/Program.cs
+++ src/TuDa.CIMS.Api/Program.cs
@@ -3,6 +3,10 @@ using TuDa.CIMS.Api;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureDbContext(builder.Configuration);
+
+
+// Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
