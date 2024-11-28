e1775b37d4fb13a5205039057e74efd76dcd400d
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2024-11-28 21:51:52 +0100

    Improve error handling with ErrorOr in IAssetItemApi

---
+++ src/TuDa.CIMS.Web/Extensions/ApiResponseExtension.cs
@@ -0,0 +1,55 @@
+﻿using Refit;
+
+namespace TuDa.CIMS.Web.Extensions;
+
+public static class ApiResponseExtension
+{
+    private static ErrorOr<TResult> ToErrorOr<TResult>(this IApiResponse<TResult> apiResponse)
+    {
+        return apiResponse.IsSuccessful switch
+        {
+            true => apiResponse.Content,
+            false => apiResponse.Error.ToError(),
+        };
+    }
+
+    private static ErrorOr<TResult> ToErrorOr<TResult>(this IApiResponse apiResponse)
+        where TResult : new()
+    {
+        return apiResponse.IsSuccessful switch
+        {
+            true => new TResult(),
+            false => apiResponse.Error.ToError(),
+        };
+    }
+
+    public static async Task<ErrorOr<TResult>> ToErrorOrAsync<TResult>(
+        this Task<IApiResponse<TResult>> apiResponseTask
+    )
+    {
+        return (await apiResponseTask).ToErrorOr();
+    }
+
+    public static async Task<ErrorOr<TResult>> ToErrorOrAsync<TResult>(
+        this Task<IApiResponse> apiResponseTask
+    )
+        where TResult : new()
+    {
+        return (await apiResponseTask).ToErrorOr<TResult>();
+    }
+
+    private static Error ToError(this ApiException exception)
+    {
+        return Error.Custom(
+            (int)exception.StatusCode,
+            exception.ReasonPhrase ?? string.Empty,
+            exception.Message,
+            new Dictionary<string, object>
+            {
+                { "url", exception.Uri.ToString() },
+                { "method", exception.HttpMethod },
+                { "data", exception.Data },
+            }
+        );
+    }
+}
