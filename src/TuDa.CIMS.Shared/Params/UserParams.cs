d2b5ab1f7c982a4219982135fdaac6832ffea1ea
david.schmidt1 <davii.schmidt@stud.tu-darmstadt.de>
2024-12-05 14:21:45 +0100

    Added class for parameter

---
+++ src/TuDa.CIMS.Shared/Params/UserParams.cs
@@ -0,0 +1,20 @@
+﻿namespace TuDa.CIMS.Shared.Params;
+
+/// <summary>
+/// A class that represents the parameters for pagination.
+/// </summary>
+public class UserParams
+{
+    private const int MaxPageSize = 50;
+    public int PageNumber { get; set; } = 1;
+    private int _pageSize = 10;
+
+    /// <summary>
+    /// A method to get and set the page size.
+    /// </summary>
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+}
