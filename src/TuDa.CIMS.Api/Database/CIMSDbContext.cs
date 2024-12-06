faadbdfb2784cb354c7feea6397c25b3426e8420
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2024-12-06 19:28:17 +0100

    Add db migration

--- src/TuDa.CIMS.Api/Database/CIMSDbContext.cs
+++ src/TuDa.CIMS.Api/Database/CIMSDbContext.cs
@@ -5,14 +5,35 @@ namespace TuDa.CIMS.Api.Database;
 
 public class CIMSDbContext : DbContext
 {
+    #region AssetItem
+
     public DbSet<AssetItem> AssetItems { get; init; }
     public DbSet<Consumable> Consumables { get; init; }
     public DbSet<Substance> Substances { get; init; }
     public DbSet<GasCylinder> GasCylinders { get; set; }
     public DbSet<Chemical> Chemicals { get; init; }
     public DbSet<Solvent> Solvents { get; set; }
+
+    #endregion
+
+    #region WorkingGroup
+
+    public DbSet<WorkingGroup> WorkingGroups { get; set; }
+    public DbSet<Professor> Professors { get; set; }
+    public DbSet<Student> Students { get; set; }
+
+    #endregion
+
+    #region Purchase
+
+    public DbSet<Purchase> Purchases { get; set; }
+    public DbSet<PurchaseEntry> PurchaseEntries { get; set; }
+
+    #endregion
+
     public DbSet<Hazard> Hazards { get; init; }
     public DbSet<Room> Rooms { get; init; }
+    public DbSet<ConsumableTransaction> ConsumableTransactions { get; set; }
 
     public CIMSDbContext(DbContextOptions<CIMSDbContext> options)
         : base(options) { }
