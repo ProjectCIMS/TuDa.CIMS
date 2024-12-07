63354313da6b9e09c5c0fab43f565b6a0e0cc04b
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2024-12-07 01:43:54 +0100

    Add Fakers for all entities

---
+++ test/TuDa.CIMS.Shared.Test/Faker/GasCylinderFaker.cs
@@ -0,0 +1,13 @@
+﻿using TuDa.CIMS.Shared.Entities;
+
+namespace TuDa.CIMS.Shared.Test.Faker;
+
+public class GasCylinderFaker : SubstanceFaker<GasCylinder>
+{
+    public GasCylinderFaker(Room? room = null, List<Hazard>? hazards = null)
+        : base(room, hazards)
+    {
+        RuleFor(g => g.Volume, f => f.Random.Double(max: 300));
+        RuleFor(g => g.Pressure, f => f.Random.Double(max: 35));
+    }
+}
