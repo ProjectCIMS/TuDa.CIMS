2dcd3b1375d0e970452dc262d51ed40a54c6e0ae
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2024-12-07 01:43:54 +0100

    Add Fakers for all entities

---
+++ test/TuDa.CIMS.Shared.Test/Faker/AssetItemFaker.cs
@@ -0,0 +1,17 @@
+﻿using TuDa.CIMS.Shared.Entities;
+
+namespace TuDa.CIMS.Shared.Test.Faker;
+
+public abstract class AssetItemFaker<TAssetItem> : BaseEntityFaker<TAssetItem>
+    where TAssetItem : AssetItem
+{
+    protected AssetItemFaker(Room? room)
+    {
+        RuleFor(c => c.Price, f => f.Random.Double(max: 100));
+        RuleFor(c => c.Room, () => room ?? new RoomFaker());
+        RuleFor(c => c.Name, f => f.Commerce.ProductMaterial());
+        RuleFor(c => c.ItemNumber, f => f.Commerce.Ean8());
+        RuleFor(c => c.Shop, f => f.Company.CompanyName());
+        RuleFor(c => c.Note, f => f.Lorem.Sentence());
+    }
+}
---
+++ test/TuDa.CIMS.Shared.Test/Faker/ConsumableTransactionFaker.cs
@@ -0,0 +1,15 @@
+﻿using TuDa.CIMS.Shared.Entities;
+using TuDa.CIMS.Shared.Entities.Enums;
+
+namespace TuDa.CIMS.Shared.Test.Faker;
+
+public class ConsumableTransactionFaker : BaseEntityFaker<ConsumableTransaction>
+{
+    public ConsumableTransactionFaker(Consumable? consumable = null)
+    {
+        RuleFor(t => t.Consumable, _ => consumable ?? new ConsumableFaker().Generate());
+        RuleFor(t => t.Date, f => f.Date.Recent());
+        RuleFor(t => t.AmountChange, f => f.Random.Int());
+        RuleFor(t => t.TransactionReason, f => f.Random.Enum<TransactionReasons>());
+    }
+}
