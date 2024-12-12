68d39c624893a2b5dfacdcd41e2be1f998f9ad6a
david.schmidt1 <davii.schmidt@stud.tu-darmstadt.de>
2024-12-12 16:16:28 +0100

    Add component

---
+++ src/TuDa.CIMS.Web/Components/ShoppingCartFooter.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using TuDa.CIMS.Shared.Entities;
+
+namespace TuDa.CIMS.Web.Components;
+
+public partial class ShoppingCartFooter(IDialogService dialogService) : ComponentBase
+{
+    [Parameter] public Purchase? Purchase { get; set; }
+
+    public double TotalPrice => Purchase?.TotalPrice ?? 0.0;
+
+    private readonly IDialogService _dialogService = dialogService;
+
+    //TODO: Implement the ConfirmAsync method
+    private Task ConfirmAsync()
+    {
+        return null;
+    }
+}
