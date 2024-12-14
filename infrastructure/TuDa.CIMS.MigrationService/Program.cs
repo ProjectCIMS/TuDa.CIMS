3dddedd7ad518533e6c974eeff9fecf591924eac
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2024-12-14 15:52:05 +0100

    Use reader in MigrationService

--- infrastructure/TuDa.CIMS.MigrationService/Program.cs
+++ infrastructure/TuDa.CIMS.MigrationService/Program.cs
@@ -2,11 +2,6 @@ using TuDa.CIMS.Api.Database;
 using TuDa.CIMS.ExcelImporter;
 using TuDa.CIMS.MigrationService;
 
-var reader = new SubstanceExcelReader();
-var solvents = reader.ReadSolvents().ToList();
-var gas = reader.ReadGasCylinders().ToList();
-var chemical = reader.ReadChemicals().ToList();
-
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.AddServiceDefaults();
