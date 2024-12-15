f2c2e9555092bcbab0175b32972b115fee7212c7
david.schmidt1 <davii.schmidt@stud.tu-darmstadt.de>
2024-12-15 18:58:02 +0100

    Add comments and minor changes

--- src/TuDa.CIMS.Web/Components/ShoppingCartFooter.razor.cs
+++ src/TuDa.CIMS.Web/Components/ShoppingCartFooter.razor.cs
@@ -4,17 +4,26 @@ using TuDa.CIMS.Shared.Entities;
 
 namespace TuDa.CIMS.Web.Components;
 
+/// <summary>
+/// Class for the shopping cart footer
+/// </summary>
+/// <param name="dialogService"></param>
 public partial class ShoppingCartFooter(IDialogService dialogService) : ComponentBase
 {
-    [Parameter] public Purchase? Purchase { get; set; }
+    /// <summary>
+    /// Parameter for the purchase
+    /// </summary>
+    [Parameter]
+    public Purchase? Purchase { get; set; }
 
+    /// <summary>
+    /// Parameter for the checkout event
+    /// </summary>
+    [Parameter]
+    public EventCallback OnCheckout { get; set; }
+
+    /// <summary>
+    /// The total price of the purchase
+    /// </summary>
     public double TotalPrice => Purchase?.TotalPrice ?? 0.0;
-
-    private readonly IDialogService _dialogService = dialogService;
-
-    //TODO: Implement the ConfirmAsync method
-    private Task ConfirmAsync()
-    {
-        return null;
-    }
 }
