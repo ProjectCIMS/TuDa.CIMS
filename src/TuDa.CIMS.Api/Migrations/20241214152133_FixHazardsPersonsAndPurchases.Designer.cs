cbc1097b7c1e283fe849c046d37e5ad0fea6c3f1
Marlon Rosenberg <59970382+Sc3l3t0n@users.noreply.github.com>
2024-12-15 16:13:12 +0100

    Fix db relations (#98)

--- src/TuDa.CIMS.Api/Migrations/20241214152133_FixHazardsPersonsAndPurchases.Designer.cs
+++ src/TuDa.CIMS.Api/Migrations/20241214152133_FixHazardsPersonsAndPurchases.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TuDa.CIMS.Api.Database;
@@ -11,9 +12,11 @@ using TuDa.CIMS.Api.Database;
 namespace TuDa.CIMS.Api.Migrations
 {
     [DbContext(typeof(CIMSDbContext))]
-    partial class CIMSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241214152133_FixHazardsPersonsAndPurchases")]
+    partial class FixHazardsPersonsAndPurchases
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
