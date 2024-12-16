3d39f0114f1255c4eb437e0f4ef22c0703051469
Thomas <thomas.kiselev@gmail.com>
2024-12-16 16:20:49 +0100

    Some changes

---
+++ src/TuDa.CIMS.Web/Components/ShoppingCartSubmitPopup.razor.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using TuDa.CIMS.Shared.Entities;
+using TuDa.CIMS.Shared.Entities.Enums;
+
+namespace TuDa.CIMS.Web.Components;
+
+public partial class ShoppingCartSubmitPopup
+{
+    /// <summary>
+    /// Returns the name of a given working group.
+    /// </summary>
+    private static string ToString(WorkingGroup workingGroup) =>
+        workingGroup switch
+        {
+            null => "",
+            _ => workingGroup.Professor.Name,
+        };
+
+    public WorkingGroup WorkingGroup { get; set; }
+
+    [Parameter] public List<WorkingGroup> WorkingGroups { get; set; }
+
+    /// <summary>
+    /// Search for the selection of the working group.
+    /// </summary>
+    private Task<IEnumerable<WorkingGroup>> Search(string searchText, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(WorkingGroups
+            .Where(w =>
+                string.IsNullOrWhiteSpace(searchText) ||
+                w.Professor.Name.Contains(searchText, StringComparison.OrdinalIgnoreCase)));
+    }
+
+    /// <summary>
+    /// CascadingParamter MudDialag.
+    /// </summary>
+    [CascadingParameter]
+    private MudDialogInstance MudDialog { get; set; }
+
+    /// <summary>
+    /// Closes the MudDialog.
+    /// </summary>
+    private void Submit() => MudDialog.Close(DialogResult.Ok(WorkingGroup));
+
+    /// <summary>
+    /// Cancels the MudDialog.
+    /// </summary>
+    private void Cancel() => MudDialog.Cancel();
+
+    /// <summary>
+    /// List of purchase entries to be shown.
+    /// </summary>
+    [Parameter]
+    public static List<PurchaseEntry> PurchaseEntries { get; set; }
+
+    /// <summary>
+    /// Returns the price unit of a given asset item as a string.
+    /// </summary>
+    private static string GetPriceText(AssetItem assetItem) =>
+        assetItem switch
+        {
+            Substance substance => $"{substance.Price}€/" + substance.PriceUnit switch
+            {
+                PriceUnits.PerKilo => "kg",
+                PriceUnits.PerLiter => "l",
+                PriceUnits.PerPiece => "Stück",
+                _ => $"{substance.Price}€ {substance.PriceUnit}"
+            },
+            _ => $"{assetItem.Price}€/Stück"
+        };
+}
