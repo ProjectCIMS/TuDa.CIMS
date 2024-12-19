ed45daf9534e83b7badbe3dbfab02b64a388fe1d
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2024-12-19 14:20:20 +0100

    Make CoverDokument customizable

---
+++ src/TuDa.CIMS.Shared/Entities/Address.cs
@@ -0,0 +1,24 @@
+﻿namespace TuDa.CIMS.Shared.Entities;
+
+public record Address
+{
+    /// <summary>
+    /// City of the address.
+    /// </summary>
+    public string City { get; set; } = "Darmstadt";
+
+    /// <summary>
+    /// Zip code of the city.
+    /// </summary>
+    public string ZipCode { get; set; } = "64387";
+
+    /// <summary>
+    /// Street of the address.
+    /// </summary>
+    public required string Street { get; set; }
+
+    /// <summary>
+    /// House number of the address.
+    /// </summary>
+    public required string HouseNumber { get; set; }
+}
