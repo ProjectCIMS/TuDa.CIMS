2043a34e193ee64f0e27cad9e01707d9ff6d7c52
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2024-12-19 15:46:32 +0100

    Improve Faker and add some extra ones

---
+++ test/TuDa.CIMS.Api.Test/Faker/InvoiceFaker.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using TuDa.CIMS.Api.Models;
+using TuDa.CIMS.Shared.Entities;
+using TuDa.CIMS.Shared.Test.Faker;
+
+namespace TuDa.CIMS.Api.Test.Faker;
+
+public class InvoiceFaker : Faker<Invoice>
+{
+    public InvoiceFaker()
+    {
+        RuleFor(i => i.Professor, () => new ProfessorFaker());
+        RuleFor(i => i.BeginDate, f => f.Date.PastDateOnly());
+        RuleFor(i => i.EndDate, f => f.Date.FutureDateOnly());
+        RuleFor(i => i.Chemicals, () => new InvoiceEntryFaker<Chemical>().GenerateBetween(0, 30));
+        RuleFor(
+            i => i.Consumables,
+            () => new InvoiceEntryFaker<Consumable>().GenerateBetween(0, 30)
+        );
+        RuleFor(i => i.Solvents, () => new InvoiceEntryFaker<Solvent>().GenerateBetween(0, 30));
+        RuleFor(
+            i => i.GasCylinders,
+            () => new InvoiceEntryFaker<GasCylinder>().GenerateBetween(0, 30)
+        );
+    }
+}
