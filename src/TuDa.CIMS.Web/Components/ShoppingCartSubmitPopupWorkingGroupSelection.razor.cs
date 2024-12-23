ac0804078c30491772cfb19d0a0176e37f7b16de
Thomas <149615614+tom03k@users.noreply.github.com>
2024-12-23 12:58:38 +0100

    Add SubmitPopup (#96)

---
+++ src/TuDa.CIMS.Web/Components/ShoppingCartSubmitPopupWorkingGroupSelection.razor.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Components;
+using TuDa.CIMS.Shared.Entities;
+using TuDa.CIMS.Shared.Entities.Enums;
+
+namespace TuDa.CIMS.Web.Components;
+
+public partial class ShoppingCartSubmitPopupWorkingGroupSelection
+{
+    /// <summary>
+    /// CascadingParameter for working group.
+    /// </summary>
+    [CascadingParameter]
+    public WorkingGroup WorkingGroup { get; set; }
+
+    /// <summary>
+    /// Search for the selection of the working group.
+    /// </summary>
+    private Task<IEnumerable<WorkingGroup>> Search(string searchText, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(WorkingGroups
+            .Where(w =>
+                string.IsNullOrWhiteSpace(searchText) ||
+                w.Professor.Name.Contains(searchText, StringComparison.OrdinalIgnoreCase)));
+    }
+
+    /// <summary>
+    /// Returns the name of a given working group.
+    /// </summary>
+    private static string ToString(WorkingGroup workingGroup) =>
+        workingGroup switch
+        {
+            null => "",
+            _ => workingGroup.Professor.Name,
+        };
+
+    /// <summary>
+    /// List of working groups.
+    /// </summary>
+    [Parameter]
+    public List<WorkingGroup> WorkingGroups { get; set; }
+}
