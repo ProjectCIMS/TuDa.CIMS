7bc6673e8a0e93f1d055012af4a02b465e4e9990
david.schmidt1 <davii.schmidt@stud.tu-darmstadt.de>
2025-01-03 12:18:00 +0100

    first draft

---
+++ src/TuDa.CIMS.Web/Components/Pages/WorkingGroup/WorkingGroupHeader.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+using TuDa.CIMS.Shared.Entities;
+
+namespace TuDa.CIMS.Web.Components.Pages.WorkingGroup;
+
+public partial class WorkingGroupHeader : ComponentBase
+{
+    [Parameter] public required Professor Professor { get; set;}
+ }
---
+++ src/TuDa.CIMS.Web/Components/Pages/WorkingGroup/WorkingGroupPurchaseList.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TuDa.CIMS.Web.Components.Pages.WorkingGroup;
+
+public partial class WorkingGroupPurchaseList : ComponentBase
+{
+}
+
