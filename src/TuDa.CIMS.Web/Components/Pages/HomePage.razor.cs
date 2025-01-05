e45c8b5999306abbc526f4931b1fc62df9a669eb
Anh-Tung <anhtung1202@live.com>
2025-01-05 17:19:31 +0100

    first refactor draft

---
+++ src/TuDa.CIMS.Web/Components/Pages/HomePage.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace TuDa.CIMS.Web.Components.Pages;
+
+public partial class HomePage
+{
+    [Inject]
+    public required IDialogService DialogService { get; set; }
+
+    private async Task OpenDialogAsync()
+    {
+        var options = new DialogOptions { CloseOnEscapeKey = true };
+        await DialogService.ShowAsync<AssetItemDialog>("Create Item", options);
+    }
+}
