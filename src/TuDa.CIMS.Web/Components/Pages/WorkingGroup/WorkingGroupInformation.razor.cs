f186bec29de4bf6d25e0fd5933341c7e800fa322
david.schmidt1 <davii.schmidt@stud.tu-darmstadt.de>
2025-01-07 16:48:25 +0100

    Added WorkingGroupInformation

---
+++ src/TuDa.CIMS.Web/Components/Pages/WorkingGroup/WorkingGroupInformation.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using TuDa.CIMS.Shared.Entities;
+
+namespace TuDa.CIMS.Web.Components.Pages.WorkingGroup;
+
+public partial class WorkingGroupInformation : ComponentBase
+{
+    [Parameter] public required  Professor ProfessorInfo { get; set; }
+}
+
