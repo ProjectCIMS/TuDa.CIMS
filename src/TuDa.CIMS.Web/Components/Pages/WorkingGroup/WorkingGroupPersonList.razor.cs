7287370c60bb7616cd9bab4228bc7f068c0578d9
david.schmidt1 <davii.schmidt@stud.tu-darmstadt.de>
2025-01-07 15:52:46 +0100

    Add draft for personlist

--- src/TuDa.CIMS.Web/Components/Pages/WorkingGroup/WorkingGroupPersonList.razor.cs
+++ src/TuDa.CIMS.Web/Components/Pages/WorkingGroup/WorkingGroupPersonList.razor.cs
@@ -7,6 +7,21 @@ public partial class WorkingGroupPersonList : ComponentBase
 {
     public List<Person> Persons{get;set;} = [];
 
+    private void EditBuyer()
+    {
+        // do something
+    }
+
+    private void RemoveBuyer()
+    {
+        // do something
+    }
+
+    private void AddBuyer()
+    {
+        // do something
+    }
+
     private List<string> items = new List<string> { "Matse Müller", "Herbert Grönemeyer" };
 
     private void AddItem()
