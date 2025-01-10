b1df1ff3e1c890a9c2c5d32ec5119bf1c60da5a7
Anh-Tung <anhtung1202@live.com>
2025-01-10 18:33:51 +0100

    first version

---
+++ src/TuDa.CIMS.Api/Interfaces/IStudentService.cs
@@ -0,0 +1,12 @@
+﻿using TuDa.CIMS.Shared.Dtos;
+using TuDa.CIMS.Shared.Entities;
+
+namespace TuDa.CIMS.Api.Interfaces;
+
+public interface IStudentService
+{
+    Task<ErrorOr<WorkingGroup>> RemoveAsync(Guid id, Guid workingGroupId);
+    Task<ErrorOr<WorkingGroup>> AddAsync(Guid id, Guid workingGroupId);
+
+    Task<ErrorOr<Updated>> UpdateAsync(Guid id, UpdateStudentDto updatedStudentDto);
+}
---
+++ src/TuDa.CIMS.Shared/Dtos/UpdateStudentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace TuDa.CIMS.Shared.Dtos;
+
+public record UpdateStudentDto
+{
+    /// <summary>
+    /// The name of the person.
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// The first name of the person.
+    /// </summary>
+    public string FirstName { get; set; } = string.Empty;
+}
