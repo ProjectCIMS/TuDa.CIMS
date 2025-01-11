6803fd78def9ed75ff25ef2f9e5b8871cacd174d
Alina <alinaweller1206@gmail.com>
2025-01-11 21:27:58 +0100

    chnages

---
+++ src/TuDa.CIMS.Shared/Dtos/CreateConsumableTransactionDto.cs
@@ -0,0 +1,30 @@
+﻿using TuDa.CIMS.Shared.Entities.Enums;
+
+namespace TuDa.CIMS.Shared.Dtos;
+
+public class CreateConsumableTransactionDto
+{
+
+    /// <summary>
+    /// The consumable of this transaction.
+    /// </summary>
+    public required Guid ConsumableId { get; set; }
+
+    /// <summary>
+    /// The date of the transaction.
+    /// </summary>
+    public required DateTime Date { get; set; }
+
+    /// <summary>
+    /// Represents the change in amount for a Consumable.
+    /// </summary>
+    /// <remarks>
+    /// Negative values indicate removal, positive values indicate addition.
+    /// </remarks>
+    public required int AmountChange { get; set; }
+
+    /// <summary>
+    /// The reason for the transaction.
+    /// </summary>
+    public required TransactionReasons TransactionReason { get; set; }
+}
