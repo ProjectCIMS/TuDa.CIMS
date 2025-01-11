baaac9dfe80ccb734e52fad151348baa9fa65306
Marlon Rosenberg <59970382+Sc3l3t0n@users.noreply.github.com>
2025-01-11 15:59:35 +0100

    Create Invoice for a DateSpan (#134)

---
+++ src/TuDa.CIMS.Api/Repositories/InvoiceRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TuDa.CIMS.Api.Database;
+using TuDa.CIMS.Api.Interfaces;
+using TuDa.CIMS.Shared.Attributes.ServiceRegistration;
+using TuDa.CIMS.Shared.Entities;
+
+namespace TuDa.CIMS.Api.Repositories;
+
+[ScopedService]
+public class InvoiceRepository : IInvoiceRepository
+{
+    private readonly CIMSDbContext _context;
+
+    public InvoiceRepository(CIMSDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<List<Purchase>> GetPurchasesInTimePeriod(
+        Guid workingGroupId,
+        DateTime beginDate,
+        DateTime endDate
+    ) =>
+        _context
+            .WorkingGroups.Where(wg => wg.Id == workingGroupId)
+            .Include(wg => wg.Purchases)
+            .SelectMany(wg => wg.Purchases)
+            .Where(p =>
+                (p.Completed && p.CompletionDate != null)
+                && (p.CompletionDate.Value >= beginDate && p.CompletionDate.Value <= endDate)
+            )
+            .Include(p => p.Entries)
+            .ThenInclude(e => e.AssetItem)
+            .Include(p => p.Buyer)
+            .ToListAsync();
+
+    public Task<Professor?> GetProfessorOfWorkingGroup(Guid workingGroupId) =>
+        _context
+            .WorkingGroups.Where(wg => wg.Id == workingGroupId)
+            .Select(wg => wg.Professor)
+            .SingleOrDefaultAsync();
+}
---
+++ src/TuDa.CIMS.Api/Services/InvoiceGenerationService.cs
@@ -0,0 +1,96 @@
+﻿using TuDa.CIMS.Api.Interfaces;
+using TuDa.CIMS.Api.Models;
+using TuDa.CIMS.Shared.Attributes.ServiceRegistration;
+using TuDa.CIMS.Shared.Entities;
+using TuDa.CIMS.Shared.Models;
+
+namespace TuDa.CIMS.Api.Services;
+
+[ScopedService]
+public class InvoiceGenerationService : IInvoiceGenerationService
+{
+    private readonly IInvoiceRepository _invoiceRepository;
+
+    public InvoiceGenerationService(IInvoiceRepository invoiceRepository)
+    {
+        _invoiceRepository = invoiceRepository;
+    }
+
+    public async Task<ErrorOr<Invoice>> CollectInvoiceForWorkingGroup(
+        Guid workingGroupId,
+        DateOnly? beginDate,
+        DateOnly? endDate
+    )
+    {
+        try
+        {
+            beginDate ??= DateOnly.MinValue;
+            endDate ??= DateOnly.MaxValue;
+
+            var professor = await _invoiceRepository.GetProfessorOfWorkingGroup(workingGroupId);
+
+            if (professor is null)
+                return Error.NotFound(
+                    $"{nameof(InvoiceGenerationService)}.{nameof(CollectInvoiceForWorkingGroup)}",
+                    $"WorkingGroup with id {workingGroupId} does not exist."
+                );
+
+            var purchasesInTimePeriod = await _invoiceRepository.GetPurchasesInTimePeriod(
+                workingGroupId,
+                beginDate.Value.ToDateTime(TimeOnly.MinValue).ToUniversalTime(),
+                endDate.Value.ToDateTime(TimeOnly.MinValue).ToUniversalTime()
+            );
+
+            var invoiceEntries = purchasesInTimePeriod
+                .SelectMany(MapPurchaseToInvoiceEntries)
+                .ToLookup(e => e.AssetItem.GetType());
+
+            return new Invoice
+            {
+                BeginDate = beginDate.Value,
+                EndDate = endDate.Value,
+                Professor = professor,
+                Consumables = invoiceEntries[typeof(Consumable)].ToList(),
+                Chemicals = invoiceEntries[typeof(Chemical)].ToList(),
+                Solvents = invoiceEntries[typeof(Solvent)].ToList(),
+                GasCylinders = invoiceEntries[typeof(GasCylinder)].ToList(),
+            };
+        }
+        catch (Exception e)
+        {
+            return Error.Failure(
+                $"{nameof(InvoiceGenerationService)}.{nameof(CollectInvoiceForWorkingGroup)}",
+                e.Message
+            );
+        }
+    }
+
+    public Task<ErrorOr<InvoiceStatistics>> GetInvoiceStatistics(
+        Guid workingGroupId,
+        DateOnly? beginDate,
+        DateOnly? endDate
+    ) =>
+        CollectInvoiceForWorkingGroup(workingGroupId, beginDate, endDate)
+            .Match(
+                value => new InvoiceStatistics
+                {
+                    TotalPriceChemicals = value.ChemicalsTotalPrice(),
+                    TotalPriceConsumables = value.ConsumablesTotalPrice(),
+                    TotalPriceSolvents = value.SolventsTotalPrice(),
+                    TotalPriceGasCylinders = value.SolventsTotalPrice(),
+                },
+                ErrorOr<InvoiceStatistics>.From
+            );
+
+    private static List<InvoiceEntry> MapPurchaseToInvoiceEntries(Purchase purchase) =>
+        purchase
+            .Entries.Select(e => new InvoiceEntry
+            {
+                AssetItem = e.AssetItem,
+                Amount = e.Amount,
+                PricePerItem = e.PricePerItem,
+                PurchaseDate = DateOnly.FromDateTime(purchase.CompletionDate!.Value),
+                Buyer = purchase.Buyer,
+            })
+            .ToList();
+}
