a5080042fdb722f52add4f5440bf9dbc7a78a0d3
Anh-Tung <anhtung1202@live.com>
2025-01-12 18:00:32 +0100

    major changes and additions

--- src/TuDa.CIMS.Web/Components/Dashboard/Dialogs/AssetItemCreateForm.razor.cs
+++ src/TuDa.CIMS.Web/Components/Dashboard/Dialogs/AssetItemCreateForm.razor.cs
@@ -2,121 +2,194 @@
 using MudBlazor;
 using TuDa.CIMS.Shared.Entities;
 using TuDa.CIMS.Shared.Entities.Enums;
+using TuDa.CIMS.Web.Services;
 
 namespace TuDa.CIMS.Web.Components.Dashboard.Dialogs;
 
 public partial class AssetItemCreateForm
 {
+    private readonly IAssetItemApi _assetItemApi;
+    private readonly ISnackbar _snackbar;
+
+    public AssetItemCreateForm(IAssetItemApi assetItemApi, ISnackbar snackbar)
+    {
+        _assetItemApi = assetItemApi;
+        _snackbar = snackbar;
+    }
+
+    public bool ShowError = false;
+    private string FeedbackMessage = string.Empty;
+    private Severity FeedbackColor = Severity.Success;
+
     /// <summary>
     /// To choose from the different forms for inputting
     /// </summary>
     private AssetItemType _selectedAssetItemType = AssetItemType.Chemical;
 
+    private AssetItemForm _assetItemForm;
+    private ChemicalItemForm _chemicalItemForm;
+    private ConsumableItemForm _consumableItemForm;
+    private GasCylinderItemForm _gasCylinderForm;
+
     /// <summary>
     /// Temporary Values to bind when inputting
-    /// TODO: maybe add set function so that price and other numerics can't be negative
     /// </summary>
-    private string Name { get; set; }
-    private string Shop { get; set; }
-    private string ItemNumber { get; set; }
-    private string Note { get; set; }
-    private string Cas { get; set; }
-    private string Manufacturer { get; set; }
-    private string SerialNumber { get; set; }
-    private double Price { get; set; }
-    private int ConsumableAmount { get; set; }
-    private string Purity { get; set; }
-    private double BindingSize { get; set; }
-    private double Volume { get; set; }
-    private double Pressure { get; set; }
-    private Room Room { get; set; }
-    private MeasurementUnits PriceUnit { get; set; }
+    private string Name { get; set; } = string.Empty;
+    private string Shop { get; set; } = string.Empty;
+    private string ItemNumber { get; set; } = string.Empty;
+    private string Note { get; set; } = string.Empty;
+    private string Cas { get; set; } = string.Empty;
+    private string Manufacturer { get; set; } = string.Empty;
+    private string SerialNumber { get; set; } = string.Empty;
+    private double Price { get; set; } = 0.0;
+    private int ConsumableAmount { get; set; } = 0;
+    private string Purity { get; set; } = string.Empty;
+    private double BindingSize { get; set; } = 0.0;
+    private double Volume { get; set; } = 0.0;
+    private double Pressure { get; set; } = 0.0;
+    private Room Room { get; set; } = new Room() { Name = string.Empty };
+    private MeasurementUnits PriceUnit { get; set; } = MeasurementUnits.Piece;
+
+    private void ResetInputs()
+    {
+        Name = string.Empty;
+        Shop = string.Empty;
+        ItemNumber = string.Empty;
+        Note = string.Empty;
+        Cas = string.Empty;
+        Manufacturer = string.Empty;
+        SerialNumber = string.Empty;
+        Price = 0.0;
+        ConsumableAmount = 0;
+        Purity = string.Empty;
+        BindingSize = 0.0;
+        Volume = 0.0;
+        Pressure = 0.0;
+        Room.Name = string.Empty;
+        ShowError = false;
+    }
 
     /// <summary>
-    /// Item after clicking "Änderungen speichern" Button
+    /// Check if all required Inputs are done
     /// </summary>
-    protected AssetItem savedItem;
+    /// <returns></returns>
+    public bool ValidateForm()
+    {
+        if (
+            _assetItemForm.ValidateForm()
+            || (
+                (
+                    _selectedAssetItemType == AssetItemType.Chemical
+                    || _selectedAssetItemType == AssetItemType.Solvent
+                ) && _chemicalItemForm.ValidateForm()
+            )
+            || (
+                _selectedAssetItemType == AssetItemType.Consumable
+                && _consumableItemForm.ValidateForm()
+            )
+            || (
+                _selectedAssetItemType == AssetItemType.GasCylinder
+                && (_gasCylinderForm.ValidateForm() || _chemicalItemForm.ValidateForm())
+            )
+        )
+        {
+            ResetInputs();
+            ShowError = true;
+            OnValidation.InvokeAsync();
+            return true;
+        }
+        OnValidation.InvokeAsync();
+        return false;
+    }
 
     [Parameter]
-    public EventCallback<AssetItem> OnSaveComplete { get; set; }
-
-    protected async Task HandleSave()
-    {
-        savedItem = SaveChanges();
-
-        if (OnSaveComplete.HasDelegate)
-        {
-            await OnSaveComplete.InvokeAsync(savedItem);
-        }
-    }
+    public EventCallback OnValidation { get; set; }
 
     /// <summary>
     /// Functionality of the "Änderungen speichern" Button: Bind the Values to the actual Item
     /// </summary>
-    protected AssetItem SaveChanges()
+    public async Task SaveChanges()
     {
-        switch (_selectedAssetItemType)
+        AssetItem? savedItem = _selectedAssetItemType switch
         {
-            case AssetItemType.Chemical:
-                return savedItem = new Chemical
-                {
-                    Name = Name,
-                    Shop = Shop,
-                    ItemNumber = ItemNumber,
-                    Note = Note,
-                    Cas = Cas,
-                    Price = Price,
-                    Purity = Purity,
-                    PriceUnit = PriceUnit,
-                    Room = Room,
-                };
+            AssetItemType.Chemical => new Chemical
+            {
+                Name = Name,
+                Shop = Shop,
+                ItemNumber = ItemNumber,
+                Note = Note,
+                Cas = Cas,
+                Price = Price,
+                Purity = Purity,
+                PriceUnit = PriceUnit,
+                Room = Room,
+            },
 
-            case AssetItemType.Consumable:
-                return savedItem = new Consumable
-                {
-                    Name = Name,
-                    Shop = Shop,
-                    ItemNumber = ItemNumber,
-                    Note = Note,
-                    Manufacturer = Manufacturer,
-                    SerialNumber = SerialNumber,
-                    Price = Price,
-                    Amount = ConsumableAmount,
-                    Room = Room,
-                };
+            AssetItemType.Consumable => new Consumable
+            {
+                Name = Name,
+                Shop = Shop,
+                ItemNumber = ItemNumber,
+                Note = Note,
+                Manufacturer = Manufacturer,
+                SerialNumber = SerialNumber,
+                Price = Price,
+                Amount = ConsumableAmount,
+                Room = Room,
+            },
 
-            case AssetItemType.GasCylinder:
-                return savedItem = new GasCylinder
-                {
-                    Name = Name,
-                    Shop = Shop,
-                    ItemNumber = ItemNumber,
-                    Note = Note,
-                    Cas = Cas,
-                    Purity = Purity,
-                    Price = Price,
-                    Volume = Volume,
-                    Pressure = Pressure,
-                    Room = Room,
-                    PriceUnit = PriceUnit,
-                };
+            AssetItemType.GasCylinder => new GasCylinder
+            {
+                Name = Name,
+                Shop = Shop,
+                ItemNumber = ItemNumber,
+                Note = Note,
+                Cas = Cas,
+                Purity = Purity,
+                Price = Price,
+                Volume = Volume,
+                Pressure = Pressure,
+                Room = Room,
+                PriceUnit = PriceUnit,
+            },
 
-            case AssetItemType.Solvent:
-                return savedItem = new Solvent
-                {
-                    Name = Name,
-                    Shop = Shop,
-                    ItemNumber = ItemNumber,
-                    Note = Note,
-                    Cas = Cas,
-                    Price = Price,
-                    Purity = Purity,
-                    Room = Room,
-                    PriceUnit = PriceUnit,
-                };
+            AssetItemType.Solvent => new Solvent
+            {
+                Name = Name,
+                Shop = Shop,
+                ItemNumber = ItemNumber,
+                Note = Note,
+                Cas = Cas,
+                Price = Price,
+                Purity = Purity,
+                Room = Room,
+                PriceUnit = PriceUnit,
+            },
 
-            default:
-                return savedItem;
+            _ => null,
+        };
+
+        if (savedItem != null)
+        {
+            try
+            {
+                //TODO: #41 needed
+                //await _assetItemApi.CreateAsync(savedItem);
+                FeedbackMessage = "Das Objekt wurde erfolgreich erstellt.";
+                FeedbackColor = Severity.Success;
+            }
+            catch (Exception ex)
+            {
+                FeedbackMessage = $"Fehler bei der Erstellung: {ex.Message}";
+                FeedbackColor = Severity.Error;
+            }
         }
+        else
+        {
+            FeedbackMessage = "Ungültiger Objekttyp.";
+            FeedbackColor = Severity.Error;
+        }
+
+        _snackbar.Add(FeedbackMessage, FeedbackColor);
     }
 }
