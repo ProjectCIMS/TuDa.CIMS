a9f6f4ade645e096fe497ea7de39d6ce2272734a
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2025-01-13 17:11:49 +0100

    Add functionality to open document in browser of user

--- src/TuDa.CIMS.Web/Services/IInvoiceApi.cs
+++ src/TuDa.CIMS.Web/Services/IInvoiceApi.cs
@@ -1,4 +1,5 @@
-﻿using Refit;
+﻿using Microsoft.JSInterop;
+using Refit;
 using TuDa.CIMS.Shared.Attributes.ServiceRegistration;
 using TuDa.CIMS.Shared.Models;
 using TuDa.CIMS.Web.Extensions;
@@ -8,7 +9,7 @@ namespace TuDa.CIMS.Web.Services;
 /// <summary>
 /// Refit client interface for performing operations on Invoices.
 /// </summary>
-[RefitClient("/api/invoices")]
+[RefitClient("/api/working-groups")]
 public interface IInvoiceApi
 {
     /// <summary>
@@ -29,7 +30,7 @@ public interface IInvoiceApi
     /// <param name="workingGroupId">The unique identifier of the working group.</param>
     /// <param name="beginDate">The optional start date of the period to retrieve statistics for.</param>
     /// <param name="endDate">The optional end date of the period to retrieve statistics for.</param>
-    [Get("/{workingGroupId}/statistics")]
+    [Get("/{workingGroupId}/invoices/statistics")]
     protected Task<IApiResponse<InvoiceStatistics>> GetStatisticsAsyncInternal(
         Guid workingGroupId,
         [Query] DateOnly? beginDate,
@@ -55,6 +56,48 @@ public interface IInvoiceApi
             .ThenAsync(content => content.ReadAsByteArrayAsync());
 
     /// <summary>
+    /// Asynchronously opens a PDF document in the browser for a specified working group and optional date range,
+    /// including additional invoice information, and triggers a download in the browser.
+    /// </summary>
+    /// <param name="workingGroupId">The unique identifier of the working group.</param>
+    /// <param name="additionalInformation">The additional invoice information to include in the PDF.</param>
+    /// <param name="jsRuntime">The JavaScript runtime to use for invoking browser download functionality.</param>
+    /// <param name="beginDate">The optional start date of the period to retrieve the PDF for.</param>
+    /// <param name="endDate">The optional end date of the period to retrieve the PDF for.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains an ErrorOr object with the success status.</returns>
+    public async Task<ErrorOr<Success>> OpenPdfAsync(
+        Guid workingGroupId,
+        AdditionalInvoiceInformation additionalInformation,
+        IJSRuntime jsRuntime,
+        DateOnly? beginDate = null,
+        DateOnly? endDate = null
+    ) =>
+        await GetPdfAsyncInternal(workingGroupId, additionalInformation, beginDate, endDate)
+            .ToErrorOrAsync()
+            .ThenAsync<HttpContent, Success>(
+                async (content) =>
+                {
+                    try
+                    {
+                        using var streamRef = new DotNetStreamReference(
+                            await content.ReadAsStreamAsync()
+                        );
+
+                        await jsRuntime.InvokeVoidAsync(
+                            "downloadFileFromStream",
+                            $"Rechnung-{additionalInformation.InvoiceNumber}.pdf",
+                            streamRef
+                        );
+                        return Result.Success;
+                    }
+                    catch (Exception e)
+                    {
+                        return Error.Failure(e.GetType().Name, e.Message);
+                    }
+                }
+            );
+
+    /// <summary>
     /// Internal method to asynchronously retrieve a PDF document for a specified working group
     /// and optional date range, including additional invoice information.
     /// </summary>
@@ -62,7 +105,7 @@ public interface IInvoiceApi
     /// <param name="additionalInformation">The additional invoice information to include in the PDF.</param>
     /// <param name="beginDate">The optional start date of the period to retrieve the PDF for.</param>
     /// <param name="endDate">The optional end date of the period to retrieve the PDF for.</param>
-    [Post("/{workingGroupId}/document")]
+    [Post("/{workingGroupId}/invoices/document")]
     protected Task<IApiResponse<HttpContent>> GetPdfAsyncInternal(
         Guid workingGroupId,
         [Body] AdditionalInvoiceInformation additionalInformation,
