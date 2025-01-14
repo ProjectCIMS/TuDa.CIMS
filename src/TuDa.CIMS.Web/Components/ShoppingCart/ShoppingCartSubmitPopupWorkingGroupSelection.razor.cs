371af3648ab9f7ff8d928a09269d16fa12301532
david.schmidt1 <davii.schmidt@stud.tu-darmstadt.de>
2025-01-14 08:04:00 +0100

    connected the SubmitPopUp to workinggroup search

--- src/TuDa.CIMS.Web/Components/ShoppingCart/ShoppingCartSubmitPopupWorkingGroupSelection.razor.cs
+++ src/TuDa.CIMS.Web/Components/ShoppingCart/ShoppingCartSubmitPopupWorkingGroupSelection.razor.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using TuDa.CIMS.Shared.Entities;
 using TuDa.CIMS.Shared.Entities.Enums;
+using TuDa.CIMS.Web.Services;
 
 namespace TuDa.CIMS.Web.Components.ShoppingCart;
 
@@ -9,27 +11,35 @@ public partial class ShoppingCartSubmitPopupWorkingGroupSelection
     [Parameter]
     public required WorkingGroup WorkingGroup { get; set; }
 
-    /// <summary>
-    /// List of working groups.
-    /// TODO: Need to be replaced by WorkingGroupApi
-    /// </summary>
-    [Parameter]
-    public List<WorkingGroup> ListOfWorkingGroups { get; set; } = [];
+    private readonly IWorkingGroupApi _workingGroupApi;
+    private MudAutocomplete<WorkingGroup> _autocomplete = null!; // Is set by blazor component
 
     /// <summary>
-    /// Search for the selection of the working group.
+    /// Event that is called when an <see cref="Shared.Entities.WorkingGroup"/> is selected.
     /// </summary>
-    private Task<IEnumerable<WorkingGroup>> Search(
-        string searchText,
-        CancellationToken cancellationToken
-    )
+    [Parameter]
+    public EventCallback<WorkingGroup> WorkingGroupSelected { get; set; }
+
+    public ShoppingCartSubmitPopupWorkingGroupSelection(IWorkingGroupApi api)
     {
-        return Task.FromResult(
-            ListOfWorkingGroups.Where(w =>
-                string.IsNullOrWhiteSpace(searchText)
-                || w.Professor.Name.Contains(searchText, StringComparison.OrdinalIgnoreCase)
-            )
-        );
+        _workingGroupApi = api;
+    }
+
+    /// Invoke to clear text
+    private async Task WorkingGroupSelectedInternal(WorkingGroup item)
+    {
+        await _autocomplete.ResetAsync();
+        await WorkingGroupSelected.InvokeAsync(item);
+    }
+
+    private async Task<IEnumerable<WorkingGroup>> Search(string name, CancellationToken token)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return [];
+        }
+
+        return await _workingGroupApi.GetAllAsync(name).Match(value => value, _ => []);
     }
 
     /// <summary>
