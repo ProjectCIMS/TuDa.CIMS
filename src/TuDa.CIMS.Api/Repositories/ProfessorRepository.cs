7660ac976b1d0149ed805c9b852a5014e4b8ff9a
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2025-01-18 01:08:14 +0100

    Create ProfessorRepository

---
+++ src/TuDa.CIMS.Api/Repositories/ProfessorRepository.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TuDa.CIMS.Api.Database;
+using TuDa.CIMS.Api.Interfaces;
+using TuDa.CIMS.Shared.Attributes.ServiceRegistration;
+using TuDa.CIMS.Shared.Dtos;
+using TuDa.CIMS.Shared.Entities;
+
+namespace TuDa.CIMS.Api.Repositories;
+
+[ScopedService]
+public class ProfessorRepository : IProfessorRepository
+{
+    private readonly CIMSDbContext _context;
+
+    public ProfessorRepository(CIMSDbContext context)
+    {
+        _context = context;
+    }
+
+    private IQueryable<Professor> ProfessorsFilledQuery =>
+        _context.Professors.Include(p => p.Address);
+
+    public Task<Professor?> GetOneAsync(Guid id) =>
+        ProfessorsFilledQuery.SingleOrDefaultAsync(p => p.Id == id);
+
+    public async Task<ErrorOr<Updated>> UpdateAsync(Guid id, UpdateProfessorDto update)
+    {
+        var professor = await GetOneAsync(id);
+
+        if (professor is null)
+        {
+            return Error.NotFound(
+                $"{nameof(ProfessorRepository)}.{nameof(UpdateAsync)}",
+                $"Professor with id {id} could not be found."
+            );
+        }
+
+        professor.Name = update.Name ?? professor.Name;
+        professor.FirstName = update.FirstName ?? professor.FirstName;
+        professor.Title = update.Title ?? professor.Title;
+        professor.Gender = update.Gender ?? professor.Gender;
+        professor.Address.City = update.AddressCity ?? professor.Address.City;
+        professor.Address.Street = update.AddressStreet ?? professor.Address.Street;
+        professor.Address.Number = update.AddressNumber ?? professor.Address.Number;
+        professor.Address.ZipCode = update.AddressZipCode ?? professor.Address.ZipCode;
+
+        await _context.SaveChangesAsync();
+
+        return Result.Updated;
+    }
+
+    public async Task<ErrorOr<Professor>> CreateAsync(CreateProfessorDto create)
+    {
+        var professor = new Professor
+        {
+            Name = create.Name,
+            FirstName = create.FirstName,
+            Gender = create.Gender,
+            Title = create.Title,
+            Address = new Address
+            {
+                Street = create.AddressStreet,
+                Number = create.AddressNumber,
+                ZipCode = create.AddressZipCode,
+                City = create.AddressCity,
+            },
+        };
+
+        await _context.Professors.AddAsync(professor);
+        await _context.SaveChangesAsync();
+
+        return professor;
+    }
+}
