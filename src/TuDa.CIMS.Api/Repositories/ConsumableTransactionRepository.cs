1bde638eb749643fdd237b70dbfa0484dde8c1c3
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2025-01-18 01:07:15 +0100

    Clean up ConsumableTransaction

--- src/TuDa.CIMS.Api/Repositories/ConsumableTransactionRepository.cs
+++ src/TuDa.CIMS.Api/Repositories/ConsumableTransactionRepository.cs
@@ -8,7 +8,7 @@ using TuDa.CIMS.Shared.Entities;
 namespace TuDa.CIMS.Api.Repositories;
 
 [ScopedService]
-public class ConsumableTransactionRepository: IConsumableTransactionRepository
+public class ConsumableTransactionRepository : IConsumableTransactionRepository
 {
     private readonly CIMSDbContext _context;
 
@@ -17,58 +17,58 @@ public class ConsumableTransactionRepository: IConsumableTransactionRepository
         _context = context;
     }
 
+    private IQueryable<ConsumableTransaction> ConsumableTransactionsFilledQuery =>
+        _context.ConsumableTransactions.Include(ct => ct.Consumable);
+
     /// <summary>
     /// Returns all existing AssetItems of the database.
     /// </summary>
-    public async Task<IEnumerable<ConsumableTransaction>> GetAllAsync()
-    {
-        return await _context.ConsumableTransactions.ToListAsync();
-    }
+    public Task<List<ConsumableTransaction>> GetAllAsync() =>
+        ConsumableTransactionsFilledQuery.ToListAsync();
 
     /// <summary>
     /// Returns an existing ConsumableTransaction with the specific id.
     /// </summary>
     /// <param name="id">the unique id of the ConsumableTransaction</param>
-    public async Task<ConsumableTransaction?> GetOneAsync(Guid id)
-    {
-        return await _context
-            .ConsumableTransactions.Where(i => i.Id == id)
-            .SingleOrDefaultAsync();
-    }
+    public Task<ConsumableTransaction?> GetOneAsync(Guid id) =>
+        ConsumableTransactionsFilledQuery.SingleOrDefaultAsync(i => i.Id == id);
 
     /// <summary>
-    /// Updates the amount of the specific Consumamble of the ConsumableTransaction and creates a new transaction for a consumable item.
+    /// Updates the amount of the specific Consumable of the ConsumableTransaction and creates a new transaction for a consumable item.
     /// </summary>
-    /// <param name="ConsumableTransactionDto"></param>
-
-    public async Task<ErrorOr<Created>> CreateAsync(CreateConsumableTransactionDto consumableTransactionDto)
+    /// <param name="consumableTransactionDto"></param>
+    public async Task<ErrorOr<Created>> CreateAsync(
+        CreateConsumableTransactionDto consumableTransactionDto
+    )
     {
-        var consumable = await _context.Consumables
-            .Where(i => i.Id == consumableTransactionDto.ConsumableId).SingleOrDefaultAsync();
-        ;
+        var consumable = await _context.Consumables.SingleOrDefaultAsync(i =>
+            i.Id == consumableTransactionDto.ConsumableId
+        );
+
         if (consumable is null)
         {
             return Error.NotFound("Consumable not found");
         }
+
         if (consumable.Amount + consumableTransactionDto.AmountChange < 0)
         {
             return Error.Failure("Negative amount of consumable is not possible.");
         }
+
         consumable.Amount += consumableTransactionDto.AmountChange;
 
+        ConsumableTransaction consumableTransaction =
+            new()
+            {
+                Date = consumableTransactionDto.Date,
+                Consumable = consumable,
+                AmountChange = consumableTransactionDto.AmountChange,
+                TransactionReason = consumableTransactionDto.TransactionReason,
+            };
 
-        ConsumableTransaction consumableTransaction = new ConsumableTransaction()
-        {
-            Date = consumableTransactionDto.Date,
-            Consumable = consumable,
-            AmountChange = consumableTransactionDto.AmountChange,
-            TransactionReason = consumableTransactionDto.TransactionReason,
-
-        };
         _context.ConsumableTransactions.Add(consumableTransaction);
         await _context.SaveChangesAsync();
+
         return Result.Created;
     }
-
-
 }
