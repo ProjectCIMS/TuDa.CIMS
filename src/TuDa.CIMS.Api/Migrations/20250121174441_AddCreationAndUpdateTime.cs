d91d565ee471d71d64be63405a9ce41fd8818c71
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2025-01-21 18:45:33 +0100

    Add fields

---
+++ src/TuDa.CIMS.Api/Migrations/20250121174441_AddCreationAndUpdateTime.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TuDa.CIMS.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreationAndUpdateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "WorkingGroups",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "WorkingGroups",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Rooms",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Rooms",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Purchases",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Purchases",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "PurchaseEntries",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "PurchaseEntries",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Persons",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Persons",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Hazards",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Hazards",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "ConsumableTransactions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "ConsumableTransactions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AssetItems",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "AssetItems",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Addresses",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Addresses",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "WorkingGroups");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "WorkingGroups");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Purchases");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Purchases");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "PurchaseEntries");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "PurchaseEntries");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Hazards");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Hazards");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "ConsumableTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "ConsumableTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AssetItems");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "AssetItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Addresses");
+        }
+    }
+}
