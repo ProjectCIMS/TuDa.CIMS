48b9b1eceaf276522ee2e7fb770f592b066cbf16
Alina <alinaweller1206@gmail.com>
2025-01-25 14:23:05 +0100

    change

--- src/TuDa.CIMS.Api/Migrations/CIMSDbContextModelSnapshot.cs
+++ src/TuDa.CIMS.Api/Migrations/CIMSDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace TuDa.CIMS.Api.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.0")
+                .HasAnnotation("ProductVersion", "9.0.1")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
@@ -190,9 +190,6 @@ namespace TuDa.CIMS.Api.Migrations
 
                     b.Property<Guid>("BuyerId")
                         .HasColumnType("uuid");
-
-                    b.Property<bool>("Completed")
-                        .HasColumnType("boolean");
 
                     b.Property<DateTime?>("CompletionDate")
                         .HasColumnType("timestamp with time zone");
