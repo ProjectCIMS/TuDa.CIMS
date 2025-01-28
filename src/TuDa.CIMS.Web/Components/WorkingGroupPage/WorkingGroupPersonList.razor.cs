54833c392f56d0e0da12f2f6fdce1d177de40c90
david.schmidt1 <davii.schmidt@stud.tu-darmstadt.de>
2025-01-28 11:14:05 +0100

    Added RemoveBuyer and fixed Refit-Client

--- src/TuDa.CIMS.Web/Components/WorkingGroupPage/WorkingGroupPersonList.razor.cs
+++ src/TuDa.CIMS.Web/Components/WorkingGroupPage/WorkingGroupPersonList.razor.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using TuDa.CIMS.Shared.Entities;
 using TuDa.CIMS.Web.Services;
 
 namespace TuDa.CIMS.Web.Components.WorkingGroupPage;
-public partial class WorkingGroupPersonList(IWorkingGroupApi workingGroupApi) : ComponentBase
+public partial class WorkingGroupPersonList(IDialogService dialogService, IWorkingGroupApi workingGroupApi, IStudentApi studentApi) : ComponentBase
 {
 
     private IEnumerable<Person> _persons = new List<Person>();
 
     [Parameter] public Guid WorkingGroupId { get; set; }
+
+    [Parameter] public EventCallback<Person> PersonDeleted { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
@@ -16,15 +19,25 @@ public partial class WorkingGroupPersonList(IWorkingGroupApi workingGroupApi) : 
         _persons = workingGroup.Value.Students;
     }
 
-    private void RemoveBuyer()
+    private async Task RemoveBuyer(Person student)
     {
-        /// Wait for api functionality
-
-        if (_persons.Any())
+        var messageBox = new MessageBoxOptions
         {
-            var modifiableList = _persons.ToList();
-            modifiableList.RemoveAt(modifiableList.Count - 1);
-            _persons = modifiableList;
+            Title = "Person löschen",
+            Message = "Wollen sie die Person wirklich löschen?",
+            YesText = "Löschen",
+            NoText = "Nein",
+        };
+        if (await dialogService.ShowMessageBox(messageBox) == true)
+        {
+            if (_persons.Any())
+            {
+                await studentApi.RemoveAsync(WorkingGroupId, student.Id);
+                var modifiableList = _persons.ToList();
+                modifiableList.Remove(student);
+                _persons = modifiableList;
+                await PersonDeleted.InvokeAsync();
+            }
         }
     }
 
