142919ba5dc84aafa35bbb617f56d44538cae6a4
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2025-02-03 21:34:57 +0100

    Remove TuDa logo

--- src/TuDa.CIMS.Api/Documents/InvoiceCoverDocument.cs
+++ src/TuDa.CIMS.Api/Documents/InvoiceCoverDocument.cs
@@ -9,11 +9,6 @@ namespace TuDa.CIMS.Api.Documents;
 public class InvoiceCoverDocument : IDocument
 {
     /// <summary>
-    /// Logo of the organization to present in the heading.
-    /// </summary>
-    private readonly Image? _logo;
-
-    /// <summary>
     /// The invoice, that should be used.
     /// </summary>
     private readonly Invoice _invoice;
@@ -27,14 +22,9 @@ public class InvoiceCoverDocument : IDocument
 
     #endregion
 
-    public InvoiceCoverDocument(
-        Invoice invoice,
-        Image? logo,
-        AdditionalInvoiceInformation information
-    )
+    public InvoiceCoverDocument(Invoice invoice, AdditionalInvoiceInformation information)
     {
         _invoice = invoice;
-        _logo = logo;
         _information = information;
     }
 
@@ -74,9 +64,6 @@ public class InvoiceCoverDocument : IDocument
                 .Row(row =>
                 {
                     row.RelativeItem().Element(ComposeProfessorSalution);
-
-                    if (_logo is not null)
-                        row.RelativeItem(0.6f).Image(_logo);
                 });
         });
     }
