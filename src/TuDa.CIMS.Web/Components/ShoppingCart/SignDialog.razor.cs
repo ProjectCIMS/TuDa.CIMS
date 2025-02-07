94026d399e91f766d9ba01b4846997222dd3fedc
Anh-Tung <anhtung1202@live.com>
2025-02-07 17:56:28 +0100

    Sign Dialog

---
+++ src/TuDa.CIMS.Web/Components/ShoppingCart/SignDialog.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SignaturePad;
+
+namespace TuDa.CIMS.Web.Components.ShoppingCart;
+
+public partial class SignDialog
+{
+    public byte[] Signature { get; set; } = Array.Empty<byte>();
+
+    [Inject]
+    private ISnackbar Snackbar { get; set; } = null!;
+
+    [CascadingParameter]
+    public required MudDialogInstance MudDialog { get; set; }
+
+    private void Submit()
+    {
+        MudDialog.Close();
+        Snackbar.Add("Der Vorgang wurde erfolgreich abgeschlossen", Severity.Success);
+    }
+
+    private void Cancel()
+    {
+        MudDialog.Cancel();
+        Snackbar.Add("Der Vorgang wurde abgebrochen", Severity.Warning);
+    }
+}
