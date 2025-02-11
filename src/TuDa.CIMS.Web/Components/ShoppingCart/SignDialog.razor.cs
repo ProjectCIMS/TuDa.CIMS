bf95062f3b0897d1265dfd51694b5e15ae0c9976
Anh-Tung Nguyen <115713229+anngy12@users.noreply.github.com>
2025-02-11 20:57:24 +0100

    Dialog for Signature (#270)

---
+++ src/TuDa.CIMS.Web/Components/ShoppingCart/SignDialog.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SignaturePad;
+
+namespace TuDa.CIMS.Web.Components.ShoppingCart;
+
+public partial class SignDialog
+{
+    private byte[] Signature { get; set; } = [];
+
+    [CascadingParameter]
+    public required MudDialogInstance MudDialog { get; set; }
+
+    private void Submit()
+    {
+        MudDialog.Close(Signature);
+    }
+
+    private void Cancel()
+    {
+        MudDialog.Cancel();
+    }
+}
