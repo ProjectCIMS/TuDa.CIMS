b742031291380eb2f74e14b8eb00a45a55938f17
Thomas <thomas.kiselev@gmail.com>
2025-02-22 13:57:43 +0100

    Insert CascadingValue

--- src/TuDa.CIMS.Web/Components/PurchaseInformation/PurchaseInformationBuyerInformation.razor.cs
+++ src/TuDa.CIMS.Web/Components/PurchaseInformation/PurchaseInformationBuyerInformation.razor.cs
@@ -1,13 +1,11 @@
-using System.Text;
 using Microsoft.AspNetCore.Components;
 using TuDa.CIMS.Shared.Dtos.Responses;
-using TuDa.CIMS.Shared.Entities;
 
 namespace TuDa.CIMS.Web.Components.PurchaseInformation;
 
 public partial class PurchaseInformationBuyerInformation
 {
-    [Parameter]
+    [CascadingParameter]
     public required PurchaseResponseDto Purchase { get; set; }
     // TODO: Dto for the signature is needed here
     //private string SignatureAsBase64 => Encoding.UTF8.GetString(Purchase.Signature);
