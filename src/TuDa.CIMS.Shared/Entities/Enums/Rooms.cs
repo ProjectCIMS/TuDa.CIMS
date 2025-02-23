34f9cce88b4ddbbe7a8b29952d38503ce1979460
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2025-02-23 15:20:02 +0100

    Add missing rooms

--- src/TuDa.CIMS.Shared/Entities/Enums/Rooms.cs
+++ src/TuDa.CIMS.Shared/Entities/Enums/Rooms.cs
@@ -2,7 +2,16 @@ namespace TuDa.CIMS.Shared.Entities.Enums;
 
 public enum Rooms
 {
+    A4,
     G17,
+    G18,
+    G19,
+    G20,
+    G21,
+    G22,
+    G23,
+    G24,
+    G25,
+    G26,
     G27,
-    A4
 }
