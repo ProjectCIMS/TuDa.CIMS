2cc9e49e770c81eeda2744c754163ceda9072c12
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2025-03-15 17:18:11 +0100

    Move around stuff and use typed DialogParameters

--- src/TuDa.CIMS.Web/Components/WorkingGroupPage/WorkingGroupPurchaseList.razor.cs
+++ src/TuDa.CIMS.Web/Components/WorkingGroupPage/WorkingGroupPurchaseList.razor.cs
@@ -9,6 +9,18 @@ namespace TuDa.CIMS.Web.Components.WorkingGroupPage;
 
 public partial class WorkingGroupPurchaseList
 {
+    /// <summary>
+    /// List of purchases that will be displayed.
+    /// </summary>
+    [Parameter]
+    public List<PurchaseResponseDto> Purchases { get; set; } = [];
+
+    /// <summary>
+    /// Working group ID to which the purchases belong to.
+    /// </summary>
+    [Parameter]
+    public Guid WorkingGroupId { get; set; }
+
     private readonly IDialogService _dialogService;
     private readonly NavigationManager _navigation;
     private readonly IPurchaseApi _purchaseApi;
@@ -24,12 +36,6 @@ public partial class WorkingGroupPurchaseList
         _purchaseApi = iPurchaseApi;
     }
 
-    [Parameter]
-    public IEnumerable<PurchaseResponseDto> Purchases { get; set; } = [];
-
-    [Parameter]
-    public Guid WorkingGroupId { get; set; }
-
     private IEnumerable<PurchaseResponseDto> SortedPurchases =>
         Purchases.OrderByDescending(p => p.CompletionDate);
 
@@ -40,29 +46,28 @@ public partial class WorkingGroupPurchaseList
     /// <returns>Returns the formatted date as string</returns>
     private static string FormatCompletionDate(PurchaseResponseDto? value)
     {
-        return value!.CompletionDate.HasValue
-            ? value
-                .CompletionDate.Value.ToLocalTime()
-                .ToString("dd.MM.yyyy HH:mm:ss", CultureInfo.GetCultureInfo("de-DE"))
-            : "";
+        return value
+                ?.CompletionDate?.ToLocalTime()
+                .ToString("dd.MM.yyyy HH:mm:ss", CultureInfo.GetCultureInfo("de-DE")) ?? "";
     }
 
     private static string GetTextStyle(PurchaseResponseDto purchase) =>
         purchase.Invalidated ? $"color: {Colors.Red.Default}; text-decoration: line-through;" : "";
 
-    private async Task NavigateToPurchase(PurchaseResponseDto purchase)
+    private async Task OpenPurchaseInfo(PurchaseResponseDto purchase)
     {
         var options = new DialogOptions { CloseOnEscapeKey = true };
         var signature = await _purchaseApi
             .RetrieveSignatureAsync(WorkingGroupId, purchase.Id)
             .Else("");
-        // Set Parameters
+
         var parameters = new DialogParameters<PurchaseInformationPopup>
         {
-            { "WorkingGroupId", WorkingGroupId },
-            { "Purchase", purchase },
-            { "SignatureAsBase64", signature.Value },
+            { popup => popup.WorkingGroupId, WorkingGroupId },
+            { popup => popup.Purchase, purchase },
+            { popup => popup.SignatureAsBase64, signature.Value },
         };
+
         await _dialogService.ShowAsync<PurchaseInformationPopup>(
             "Rechnungsinformationen",
             parameters,
